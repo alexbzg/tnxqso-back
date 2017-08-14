99bf222e4eb0258805b86fcca1d64311c80abc4e
root <alexbzg@gmail.com>
2017-08-14 17:27:51 +0300

    recaptcha

---
+++ common.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+#coding=utf-8
+
+
+import configparser, decimal, json, logging
+from os import path
+from datetime import datetime, date
+
+appRoot = path.dirname( path.abspath( __file__ ) ) 
+
+def siteConf():
+    conf = configparser.ConfigParser()
+    conf.optionxform = str
+    conf.read( appRoot + '/site.conf' )
+    return conf
+
+def readConf( file ):
+    conf = configparser.ConfigParser()
+    conf.read( appRoot + '/' + file )
+    return conf
+
+
+def jsonEncodeExtra( obj ):
+    if isinstance( obj, decimal.Decimal ):
+        return float( obj )
+    elif isinstance(obj, datetime):
+        return obj.isoformat()
+    elif isinstance(obj, date):
+        return obj.isoformat()
+    raise TypeError( repr( obj ) + " is not JSON serializable" )
+
+def loadJSON( pathJS ):
+    if not path.isfile( pathJS ):
+        logging.exception( pathJS + " not found" )
+        return False
+    try:
+        r = json.load( open( pathJS ) )
+        return r
+    except Exception as ex:
+        logging.error( "Error loading " + pathJS )
+        logging.exception( ex )
+        return False
+
+
