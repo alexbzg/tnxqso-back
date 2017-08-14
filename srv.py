c313f9ee00711acdfcecf42f354b875d47551ea7
root <alexbzg@gmail.com>
2017-08-14 16:47:17 +0300

    recaptcha

--- srv.py
+++ srv.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 #coding=utf-8
 
-import argparse, asyncio, logging, logging.handlers
+import argparse, asyncio, logging, logging.handlers, aiohttp
 from aiohttp import web
+from common import siteConf
 
 parser = argparse.ArgumentParser(description="tnxqso backend aiohttp server")
 parser.add_argument('--path')
@@ -17,10 +18,45 @@ loggerHandler.setFormatter( logging.Formatter( \
 logger.addHandler( loggerHandler )
 logger.debug( "restart" )
 
+conf = siteConf()
+
+@asyncio.coroutine
+def checkRecaptcha( response ):
+    try:
+        rcData = { 'secret': conf.get( 'recaptcha', 'secret' ),\
+                'response': response }
+        with aiohttp.ClientSession() as session:
+            resp = yield from session.post( conf.get( 'recaptcha', 'verifyURL' ), data = rcData )
+            respData = yield from resp.json()
+            return respData['success']
+    except Exception:
+        logging.exception( 'Recaptcha error' )
+        return False
+
+def getUserData( callsign, password ):
+    return { 'callsign': callsign }
+
 @asyncio.coroutine
 def loginHandler(request):
+    error = None
     data = yield from request.json()
-    return web.json_response(data)
+    if not data.has_key( 'login' ) or len( data['login'] ) < 2:
+        error = 'Minimal login length is 2 symbols'
+    if not data.has_key( 'password' ) or len( data['password'] ) < 6:
+        error = 'Minimal password length is 6 symbols'
+    if not error:
+        if data['newUser']:
+            rcTest = yield from checkRecaptcha( data['recaptcha'] )
+            if not rcTest:
+                error = 'Recaptcha test failed. Please try again'
+            else:
+                continue
+        else:
+            continue
+    if error:
+        return web.HTTPBadRequest( text = error )
+    else:
+        return web.json_response( getUserData( data['login'] ) )
 
 
 if __name__ == '__main__':
