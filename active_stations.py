d46bfb97ebb160b3d17159a81a73c7f1664b124a
root <root@194-67-78-213.cloudvps.regruhosting.ru>
2022-08-30 12:50:55 +0300

    feat: active_stations invalidate online/freq fields on station timeout

--- active_stations.py
+++ active_stations.py
@@ -4,6 +4,7 @@ import logging
 import logging.handlers
 from datetime import datetime
 import json
+import time
 
 from common import siteConf, loadJSON, startLogging
 
@@ -15,6 +16,10 @@ publish = loadJSON(f'{webRoot}/js/publish.json')
 
 data = []
 today = datetime.utcnow().date()
+now = time.time()
+
+ONLINE_INT = 120
+FREQ_INT = 300
 
 for callsign, publishSettings in publish.items():
     if all(publishSettings.values()):
@@ -26,9 +31,18 @@ for callsign, publishSettings in publish.items():
             activityPeriod = [datetime.strptime(item, '%d.%m.%Y').date()
                     for item in stationSettings['station']['activityPeriod'] if item is not None]
             if len(activityPeriod) == 2 and activityPeriod[0] <= today <= activityPeriod[1]:
+                status = loadJSON(f'{stationPath}/status.json')
+                if status.get('online') and stationSettings['status']['get'] != 'manual':
+                    status['online'] = now - status['ts'] < ONLINE_INT
+                else:
+                    status['online'] = status.get('online')
+                if status.get('freq') and status['freq'].get('value'):
+                    status['freq'] = status['freq']['value'] if now - status['freq']['ts'] < FREQ_INT else None
+                else: 
+                    status['freq'] = None
                 data.append({
                     'callsign': callsign,
-                    'status': loadJSON(f'{stationPath}/status.json')
+                    'status': status
                     })
 
 data.sort(key=lambda item: item['callsign'])
