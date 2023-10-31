71acf8f04ca5fa2e503ff30f71ade41b011d685a
root <root@194-67-78-213.cloudvps.regruhosting.ru>
2023-10-31 13:32:28 +0300

    tests: cluster gateway

---
+++ test/test_cluster.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+#coding=utf-8
+import json
+import logging
+import asyncio
+
+import httpx
+import pytest
+
+from tnxqso.common import WEB_ADDRESS
+
+SPOT_CALLSIGN = "R1CF/M"
+USER_CALLSIGN = "R7CL"
+FREQ = "7080,0"
+
+@pytest.mark.asyncio
+async def test_valid_requests():
+    async with httpx.AsyncClient() as client:
+        payload = {
+                "cs": SPOT_CALLSIGN,
+                "userCS": USER_CALLSIGN,
+                "freq": FREQ,
+                "info": "www.TNXQSO.com"
+                }
+        
+        rsp = await client.post(f"{WEB_ADDRESS}/aiohttp/sendSpot", json=payload)
+        rsp.raise_for_status()
+        rsp_data = json.loads(rsp.text)
+        logging.debug(rsp_data)
+        assert rsp_data['sent']
+        
+        rsp = await client.post(f"{WEB_ADDRESS}/aiohttp/sendSpot", json=payload)
+        rsp.raise_for_status()
+        rsp_data = json.loads(rsp.text)
+        logging.debug(rsp_data)
+        assert not rsp_data['sent']
+        assert rsp_data['secondsLeft'] > 0
+
+        await asyncio.sleep(rsp_data['secondsLeft'])
+        
+        rsp = await client.post(f"{WEB_ADDRESS}/aiohttp/sendSpot", json=payload)
+        rsp.raise_for_status()
+        rsp_data = json.loads(rsp.text)
+        logging.debug(rsp_data)
+        assert rsp_data['sent']
+
+@pytest.mark.asyncio
+async def test_invalid_callsign():
+    async with httpx.AsyncClient() as client:
+        payload = {
+                "cs": SPOT_CALLSIGN,
+                "userCS": "RRRRRRRRRRRRRRRRRR",
+                "freq": FREQ,
+                "info": "www.TNXQSO.com"
+                }
+        
+        rsp = await client.post(f"{WEB_ADDRESS}/aiohttp/sendSpot", json=payload,
+                timeout=30)
+        rsp.raise_for_status()
+        rsp_data = json.loads(rsp.text)
+        logging.debug(rsp_data)
+        assert not rsp_data['sent']
+        assert rsp_data['reply']
--- tnxqso/routes/cluster.py
+++ tnxqso/routes/cluster.py
@@ -11,6 +11,7 @@ from tnxqso.common import CONF
 from tnxqso.services.auth import auth
 
 CLUSTER_ROUTES = web.RouteTableDef()
+RETRIES_LIMIT = 3
 
 @CLUSTER_ROUTES.post('/aiohttp/sendSpot')
 @auth(require_token=False)
@@ -19,8 +20,10 @@ async def send_spot_handler(data, *, request, **_):
     response = {'sent': False,
             'secondsLeft': CONF.getint('cluster', 'spotInterval')}
     if now - request.app['tnxqso-last-spot-sent'][data['cs']] > response['secondsLeft']:
+        request.app['tnxqso-last-spot-sent'][data['cs']] = now
         retries = 0
-        while retries < 3:
+        last_reply = ''
+        while retries < RETRIES_LIMIT:
             try:
                 fut_connection = asyncio.open_connection(
                         CONF.get('cluster', 'host'),
@@ -32,24 +35,27 @@ async def send_spot_handler(data, *, request, **_):
                     writer.write(f"{cmd}\n".encode())
 
                 async def login():
+                    nonlocal last_reply
                     logged_in = False
                     while not logged_in:
-                        cluster_rsp = (await reader.readline()).decode().strip()
-                        if 'your call:' in cluster_rsp:
-                            write(data['userCS'])
-                        elif 'Please enter' in cluster_rsp:
-                            write('-')
-                        elif '(Y or N)' in cluster_rsp:
-                            write('Y')
-                        elif '>' in cluster_rsp:
-                            logged_in = True
+                        cluster_reply = (await reader.readline()).decode().strip()
+                        if cluster_reply:
+                            last_reply = cluster_reply
+                            if 'your call:' in cluster_reply:
+                                write(data['userCS'])
+                            elif 'Please enter' in cluster_reply:
+                                write('-')
+                            elif '(Y or N)' in cluster_reply:
+                                write('Y')
+                            elif '>' in cluster_reply:
+                                logged_in = True
 
                 await asyncio.wait_for(login(), timeout=2)
 
                 write(f"dx {data['cs']} {data['freq']} {data['info']}")
-                request.app['tnxqso-last-spot-sent'][data['cs']] = now
                 response['sent'] = True
                 await asyncio.sleep(0.5)
+                request.app['tnxqso-last-spot-sent'][data['cs']] = time.time()
                 break
             except (OSError, asyncio.TimeoutError) as exc:
                 logging.error('Cluster connect error %s', exc)
@@ -57,8 +63,10 @@ async def send_spot_handler(data, *, request, **_):
             finally:
                 if writer:
                     writer.close()
-        if retries == 3:
+        if retries == RETRIES_LIMIT:
             logging.error('Connection retries limit reached')
+            if last_reply:
+                response['reply'] = last_reply
     else:
-        response['secondsLeft'] -= now - request.app['tnxqso-last-spot-sent'][data['cs']]
+        response['secondsLeft'] -= time.time() - request.app['tnxqso-last-spot-sent'][data['cs']]
     return web.json_response(response)
